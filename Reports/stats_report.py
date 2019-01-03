2e838189c9a09e1eba1e6f3d054f5cd7fb6d1fa4
jadsdad <simon.c.slator@outlook.com>
2019-01-03 21:33:30 +0000

    Fixed time calculation in general section

--- Reports/stats_report.py
+++ Reports/stats_report.py
@@ -679,12 +679,14 @@ def total_albums_played():
     return results[0][0]
 
 def total_time():
-    sql = "SELECT SUM(length) FROM track;"
+    sql = "SELECT SUM(tracklength) FROM tracklengths inner join album on tracklengths.albumid = album.albumid " \
+          "where album.sourceid<>6;"
     results = get_results(sql)
     return results[0][0]
 
 def total_excl_bonus():
-    sql = "SELECT SUM(length) FROM track where BonusTrack = 0;"
+    sql = "SELECT SUM(tracklength) FROM tracklengths inner join album on tracklengths.albumid = album.albumid " \
+          "where album.sourceid<> 6 and BonusTrack = 0;"
     results = get_results(sql)
     return results[0][0]
 
