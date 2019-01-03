1458d3724d2d3e925bd5b7a211b83efbef69dadb
jadsdad <simon.c.slator@outlook.com>
2019-01-03 19:31:10 +0000

    Fixed typo

--- Reports/yoy_comparison_graph.py
+++ Reports/yoy_comparison_graph.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 from datetime import date
 
-conn = MariaDB.connect(db='catalogue', use_unicode=True, charset='utf8')
+conn = mariadb.connect(db='catalogue', use_unicode=True, charset='utf8')
 
 def query_db(sql):
     cursor = conn.cursor()
